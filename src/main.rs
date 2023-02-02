70ef6735e44b471970ca38e6d4568f5b6b10a3ba
Martin Junghanns <m.junghanns@mailbox.org>
2023-02-02 18:28:49 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::io::Write;
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
+use std::io::{stdin, stdout, Read, Write};
 use std::path::Path;
 
 type AppResult = Result<(), std::io::Error>;
@@ -27,33 +31,64 @@ fn run_file(file: impl AsRef<Path>) -> AppResult {
 }
 
 fn run_repl() -> AppResult {
-    let stdin = std::io::stdin();
+    let stdin = stdin();
+    let mut stdout = stdout().into_raw_mode()?;
+
     let mut line = String::new();
+    let prompt = ">";
+
+    write!(
+        stdout,
+        "{}{}{}>{}",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1),
+        termion::cursor::Hide,
+        termion::cursor::Goto(3, 1),
+    )?;
 
     loop {
-        let mut prompt = "> ";
-        line.clear();
+        let stdin = stdin.lock();
+        let mut stdout = stdout.lock();
 
-        loop {
-            print!("{prompt}");
-            std::io::stdout().flush()?;
-            prompt = ". ";
-            stdin.read_line(&mut line)?;
-            line = line.trim().to_string();
+        stdout.flush()?;
 
-            if !line.ends_with('\\') {
-                break;
+        for c in stdin.keys() {
+            match c? {
+                Key::Ctrl('c') => return Ok(()),
+                Key::Char('\n') => break,
+                Key::Char(c) => {
+                    line.push(c);
+                    write!(stdout, "{}", c)?
+                }
+                _ => {}
             }
-
-            line = line.trim_end_matches('\\').to_string();
+            stdout.flush()?;
         }
+
+        stdout.flush()?;
+
+        // loop {
+        //     // print!("{prompt}");
+        //     // stdout.flush()?;
+        //     prompt = ". ";
+        //     stdin.read_line(&mut line)?;
+        //     line = line.trim().to_string();
+
+        //     if !line.ends_with('\\') {
+        //         break;
+        //     }
+
+        //     line = line.trim_end_matches('\\').to_string();
+        // }
 
         if line == "quit" {
             break;
         }
 
         if let Err(bollox_errors) = bollox::run(&line) {
-            println!("{:?}", miette::Report::new(bollox_errors))
+            let mut stdout = std::io::stdout();
+            // println!("{:?}", miette::Report::new(bollox_errors));
+            write!(stdout, "{:?}", miette::Report::new(bollox_errors))?;
         }
     }
     Ok(())
